8d93da6c82eee6f598b53d1ac88e5826f4545f1e
Carter Allen <bcarter.allen1@gmail.com>
2018-11-01 17:00:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ blue_intensity= 200 # Frequency of blue light
 Root_File_Name = "StageMaster_Pictures/"
 
 ## System Setup ##
-endstop_pins=[5,6,13,19,26,20]
-camera_pins=[4,17,18]
-rgb_pins=[23,24,25]
-lighting = pigpio.pi()
+endstop_pins=[5,6,13,19,26,20] #GPIO pin numbers defined for photogate sensors 
+camera_pins=[4,17,18]#GPIO pins used by camera apparatus
+rgb_pins=[23,24,25]#GPIO pins used for the led strip
+lighting = pigpio.pi() #instances the lighting object
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -26,15 +26,15 @@ for i in range(3):
 for i in range(6):
     GPIO.setup(endstop_pins[i], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# create a default object, no changes to I2C address or frequency
-mh = Adafruit_MotorHAT(addr = 0x60)
-LinearMotor = mh.getStepper(200, 1)
-LinearMotor.setSpeed(30)
-RotaryMotor = mh.getStepper(200,2)
-RotaryMotor.setSpeed(2)
+# creates a default motor object, no changes to I2C address or frequency
+mh = Adafruit_MotorHAT(addr = 0x60) #instances the motor hat object
+LinearMotor = mh.getStepper(200, 1) #instances the linear motor object, with 200 steps/rev, motor port 1
+LinearMotor.setSpeed(30) #Sets speed to 30 RPM (approx. due to raspberry pi)
+RotaryMotor = mh.getStepper(200,2) #instance rotary motor, 200 steps/rev, motor port 2
+RotaryMotor.setSpeed(2) #2 RPM
 
 
-## Temp Test Functions ##
+## Peripheral Functions ##
 
 #Turn on lights, here is where the lighting ratios can be modified
 def turnOnLights():
@@ -42,26 +42,25 @@ def turnOnLights():
     lighting.set_PWM_dutycycle(rgb_pins[1],green_intensity)
     lighting.set_PWM_dutycycle(rgb_pins[2],blue_intensity)
 
-#Turn of all lights and stop pigpio
+#Turn off all lights and stop pigpio
 def turnOffLights():
     for i in range(3):
         lighting.set_PWM_dutycycle(rgb_pins[i], 0)
-
-#Makes a .txt file to reset number of trials for vial number reference and returns .txtfile name
+#Makes a .txt file to reset number of trials for vial number reference and returns filepath string
 def initial_trial_count(run_path):
-    Trial_Count= open(run_path+"/Trial_Count.txt", "w")
-    Trial_Count.write("1")
+    Trial_Count= open(run_path+"/Trial_Count.txt", "w") #creates a new folder, or opens existing for write
+    Trial_Count.write("1") #writes a 1 to this 
     Trial_Count.close()
     trial_path=run_path+"/Trial_Count.txt"
     return(trial_path)
 
-#Reads the trial .txt file, converts into an integer, and then increments the .txt file
+#Reads the trial .txt file, converts into an integer, and then returns integer # of trials
 def read_trial_count(trial_path):
     Trial_Count = open(trial_path, "r")
     current_trial = int(Trial_Count.read())
     Trial_Count.close()
     return(current_trial)
-
+#Increments the trial count # by 1
 def inc_trial_count(trial_path):
     Trial_Count = open(trial_path, "r")
     current_trial = int(Trial_Count.read())
@@ -79,8 +78,8 @@ def turnOffMotors():
     mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
     mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
-#def detect(endstop)
-def detect(endstop): # Returns boolean value for whether a specific GPIO pin is obstructed
+#Function that returns a boolean value for whether a specific GPIO pin is obstructed
+def detect(endstop):
     if GPIO.input(endstop) == True:
         return(1)
     else:
@@ -88,48 +87,47 @@ def detect(endstop): # Returns boolean value for whether a specific GPIO pin is 
 #Function that moves the endstop right to endstop_number (1-6)
 def move_linear_platform(endstop_number): 
     endstop=endstop_pins[endstop_number - 1]
-    if detect(endstop_pins[5])== 1:
+    if detect(endstop_pins[5])== 1: #if the platform is all the way at the end, stop 
+        print("The platform is at the last endstop")
         turnOffMotors()
-    while detect(endstop)== 0:
+    while detect(endstop)== 0:#move platform forward until it reaches endstop
         LinearMotor.oneStep(Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.INTERLEAVE)
-    while detect(endstop)== 1:
+    while detect(endstop)== 1:#move platform forward until notch in flag to center camera
         LinearMotor.oneStep(Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.INTERLEAVE)
     turnOffMotors()
     print ("Platform Centered")
 
-def reset_linear_platform():
+def reset_linear_platform(): 
     if detect(endstop_pins[0])== 1:
         print("At endstop 1")
         turnOffMotors()
-    while detect(endstop_pins[0])==0:
+    while detect(endstop_pins[0])==0:#move platform back to start
         LinearMotor.oneStep(Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.INTERLEAVE)
-    while detect(endstop_pins[0])==1:
+    while detect(endstop_pins[0])==1:#center at notch
         LinearMotor.oneStep(Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.INTERLEAVE)
     turnOffMotors()
     print ("linear platform reset")
 
-#takes a picture with Camera 1 and stores
-#the .jpg in "file" (path), then takes a picture with Camera 2
-#and does the same
+#Function takes a picture with Camera 1 and stores the .jpg in "file" (path), then takes a picture with Camera 2 and does the same
 def take_picture_cameras(file):
-    GPIO.output(camera_pins[0],False)
+    GPIO.output(camera_pins[0],False)#Initialize pins for camera 1
     GPIO.output(camera_pins[1],False)
     GPIO.output(camera_pins[2],True)
     timestr1 = time.strftime("/C1_D%Y_%m_%d-T%H_%M_%S")
     #make_dir=time.strftime("Camera1_D_%Y_%m_%d_T_%H_%M")
     capture1="raspistill -t 500 -st -o %s.jpg" % (file+timestr1)
-    os.system(capture1)
+    os.system(capture1) #Utilize linux command line to get Raspberry pi to take picture with no delay
     print("Camera 1 Capture Succesful")
-    GPIO.output(camera_pins[0],True)
+    GPIO.output(camera_pins[0],True) #Initialize pins for camera 2
     GPIO.output(camera_pins[1],False)
     GPIO.output(camera_pins[2],True)
     timestr2 = time.strftime("/C2_D%Y_%m_%d-T%H_%M_%S")
     capture2 = "raspistill -t 500 -st -o %s.jpg" % (file+timestr2)
-    os.system(capture2)
+    os.system(capture2)#Utilize linux command line to take pic no delay camera 2
     print("Camera 2 Capture Successful")
 
 def rotate_steps(n_steps):
-    for i in range(int(n_steps)):
+    for i in range(int(n_steps)-1): #rotate certain number of steps
         RotaryMotor.oneStep(Adafruit_MotorHAT.FORWARD,Adafruit_MotorHAT.MICROSTEP)
 
 def rotate_vial(interval):
@@ -138,7 +136,7 @@ def rotate_vial(interval):
     print ("vial rotated by", interval)
 
 def rotate_reset():
-    for i in range(int(1600)):
+    for i in range(int(1600)-1):
         RotaryMotor.oneStep(Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.MICROSTEP)
     print("Vial Rotated to Reset")
 
@@ -150,33 +148,30 @@ def input_cmd(): ## returns object with parameters
     time = float(input("Length of Operation (mins): "))
     n_reps = int(input("Number of repetitions in length of operation: "))
     #store_path = str(raw_input("Path (folder in home dir): "))
-
     return ob.Operation_Input(n_vials, steps, time, n_reps)
-def root_file_gen(root_file_name):
+
+def root_file_gen(root_file_name): #Initializes a root file in the pi home directory
     bin_path = "/home/pi/"
     newpath = bin_path + root_file_name
     if not os.path.exists(newpath):
-        os.makedirs(newpath)
-    
+        os.makedirs(newpath)   
     return (newpath)
 
-def run_file_gen(rootpath):
+def run_file_gen(rootpath): #Initializes a time dependent run file containing all pictures of all repetitions of all vials
     run_folder = time.strftime("Run_%Y_%m_%d-%H:%M")
     newpath = rootpath+run_folder
     if not os.path.exists(newpath):
         os.makedirs(newpath)
-
     return (newpath)
 
-def vial_file_gen(rootpath, n_vial):
+def vial_file_gen(rootpath, n_vial): # Initializes Branch from run file containing all Trials of each vial
     vial_folder = ("Vial_" + str(n_vial))
     rootpath = os.path.join(rootpath, vial_folder)
     if not os.path.exists(rootpath):
         os.makedirs(rootpath)
-
     return (rootpath)
 
-def routine_file_gen(vialpath, Trial_Count):
+def routine_file_gen(vialpath, Trial_Count): # Initializes branch from vial containing camera 1 and camera 2 for a trial and vial
     routine_folder = ("Trial_" + str(Trial_Count))
     vialpath = os.path.join(vialpath, routine_folder)
     if not os.path.exists(vialpath):
@@ -195,11 +190,11 @@ def operation(input_ob,rootpath):
     interval = math.floor(1600 / steps)
     remainder = 1600 - (steps * interval)
     reset_linear_platform()
-    for i in range(n_vials):
+    for i in range(n_vials): #generate vialpaths for operation
         vialpath = vial_file_gen(rootpath, i+1)    
         next_endstop_number=2+i
         trial_folder = routine_file_gen(vialpath, trial_count)
-        for j in range(steps):
+        for j in range(steps): #run a procedure of taking pictures while lit, rotating, taking pics
             turnOnLights()
             take_picture_cameras(trial_folder)
             rotate_vial(interval)
